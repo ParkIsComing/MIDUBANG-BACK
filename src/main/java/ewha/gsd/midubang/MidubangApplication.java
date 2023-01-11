736d8f88867acfe95833e06714ff528973d2cd9f
ParkIsComing <maroony55@gmail.com>
2023-01-11 20:30:35 +0900

    initial commit

---
+++ src/main/java/ewha/gsd/midubang/MidubangApplication.java
@@ -0,0 +1,13 @@
+package ewha.gsd.midubang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidubangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidubangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ewha/gsd/midubang/MidubangApplicationTests.java
@@ -0,0 +1,13 @@
+package ewha.gsd.midubang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidubangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
